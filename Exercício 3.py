058f91c965f0cdf0d7ac818db7140dc908c80427
Robson Drumond <hensbr@gmail.com>
2022-09-29 21:42:13 -0300

    Exercício 3
    Recebe a entrada de 2 valores e realiza a soma imprimindo o resultado.

---
+++ Exercício 3.py
@@ -0,0 +1,5 @@
+#Exercício 3 (Soma de 2 valores)
+valor_1 = int(input('Digite o primeiro valor: '))
+valor_2 = int(input('Digite o primeiro valor: '))
+resultado = valor_1 + valor_2
+print(f' A soma dos valores é: ', resultado)
