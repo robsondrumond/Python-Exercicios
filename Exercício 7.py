195264ddccff66b9bf24c432ad166b24dcf14e36
Robson Drumond <hensbr@gmail.com>
2022-09-29 21:49:52 -0300

    Exercício 7
    Recebe a entrada de 4 notas e imprime a média delas.

---
+++ Exercício 7.py
@@ -0,0 +1,7 @@
+Exercício 7 (Calculo de média de nota)
+nota_1 = float(input('Digite a primeira nota '))
+nota_2 = float(input('Digite a segunda nota '))
+nota_3 = float(input('Digite a terceira nota '))
+nota_4 = float(input('Digite a quarta nota '))
+media = (nota_1 + nota_2 + nota_3 + nota_4) / 4
+print('A média do aluno no ano foi {}.', format(media))
