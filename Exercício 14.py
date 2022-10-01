01dbe04be262ef31703586b84f22b89157237e4b
Robson Drumond <hensbr@gmail.com>
2022-09-30 21:18:00 -0300

    Exercício 14
    Recebe a entrada da 'temperatura' em Celcius e calcula para Fahrenheit

---
+++ Exercício 14.py
@@ -0,0 +1,4 @@
+#Exercício 14 (Conversão de temperatura)
+celcius = float(input('Informe a temperatura em celcius: '))
+fahrenheit = (celcius * 1.8) + 32
+print('A temperatura de {}ºC, corresponde a {}ºF.' .format(celcius, fahrenheit))
