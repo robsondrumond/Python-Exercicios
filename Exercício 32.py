2903c4606844579c598b703114872dabbaaa00d5
Robson Drumond <hensbr@gmail.com>
2022-10-01 10:34:47 -0300

    Exercício 32
    Recebe entrada de um ano e informa se é bissexto ou não

---
+++ Exercício 32.py
@@ -0,0 +1,6 @@
+#Exercício 32 (Recebe entrada de um ano e informa se é bissexto ou não)
+ano = int(input('Digite o ano: '))
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('O ano de {} é bissexto!'.format(ano))
+else:
+    print('O ano de {} não é bissexto!'.format(ano))
